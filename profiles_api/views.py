bdfee94057748bd36c1ff497296265f7364a625b
ibadathossain <u1704025@student.cuet.ac.bd>
2023-11-28 06:44:28 +0600

    Added HelloApiView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+class HelloApiView(APIView):
+    def get(self, request, format=None):
+        an_apiview = [
+            'this is the most common things',
+            'hello i am ibadat hossain',
+            'ki chai tor',
+        ]
+        return Response({'message':'Hello!','an_apiview':an_apiview})
