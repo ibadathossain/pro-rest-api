f1969f9faa541327acae738b0a363cbeaf43eae0
ibadathossain <u1704025@student.cuet.ac.bd>
2023-11-28 09:29:09 +0600

    Added viewset yo profile api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 from profiles_api import serializers
@@ -34,3 +35,39 @@ class HelloApiView(APIView):
 
     def delete(self, request, pk = None):
         return Response({'methods':'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    serializer_class = serializers.HelloSerializer
+    def list(self,request):
+        a_viewset = [
+            'haha what are you doing',
+            'no this is not possible',
+        ]
+        return Response({'message':'Hello!', 'a_viewset' : a_viewset})
+
+    def create(self,request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self,request,pk=None):
+        return Response({'http_method':'GET'})
+
+
+    def update(self,request,pk=None):
+        return Response({'http_method':'PUT'})
+
+    def partial_update(self,request,pk=None):
+        return Response({'http_method':'PATCH'})
+
+    def destroy(self,request,pk=None):
+        return Response({'http_method':'DELETE'})
+
